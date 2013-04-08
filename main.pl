65c93feb958f5f9d226f64741a9319fee34e812b
Hiroshi Ueki <petykin+github@gmail.com>
2013-04-08 15:57:57 +0900

    元サンプルからの変更点
    ・質問部分をサブルーチン化
    ・値チェックをサブルーチン化、値が不正なときは再度質問するように変更
    ・値チェック時、２文字以上を対象とするように変更
    ・値チェック時、大文字小文字を区別して比較するように変更

--- main.pl
+++ main.pl
@@ -1,42 +1,30 @@
 use strict;
 use warnings;
 
-# scalar
 my $title = "true tears で学ぶ、Perl入門 改";
 
-# List
 my @names = qw(Shinichirou Noe Hiromi Aiko Nobuse Yusukebe);
 
 question();
 
 sub question{
-  # std io
-  print "Dareni suru?\n";
-
-	# function
+	print "Dareni suru?\n";
 	print join( ', ', @names)."\n";
 
-	# std io
 	my $name = <STDIN>;
 
-	# function
 	chomp($name);
 
-	# error
 	die "真一郎の靴の底にもあぶらむし\n" unless $name;
 
 	validation($name);
-
 }
 
 
 sub validation{
 	my $name = shift;
-	# 制御構文, grep, 正規表現
-
 	# +++ １文字だと、その文字を含む名前がマッチするので、２文字以上を要求
 	if (length($name)>1 && grep{ $_ =~ /$name/ } @names){
-	# --- if (grep{ $_ =~ /$name/i } @names){
 		print "$name: ". talk($name) . "\n";
 	}
 	else{
@@ -45,10 +33,8 @@ sub validation{
 	}
 }
 
-# sub routine
 sub talk {
 	my $name = shift;
-	# reference
 	my $map = {
 		Shinichirou	=>	['全部ちゃんとするから','君の涙を'],
 		Noe 		=>	['私、涙、あげちゃったから','はい、ぱちぱちってして'],
@@ -58,9 +44,6 @@ sub talk {
 		Yusukebe	=>	['メルマガよろしく！'],
 	};
 
-	# dereference
 	my @sentences = @{ $map->{$name} };
-
-	# list grep
 	return $sentences[ rand @sentences ];
 }
